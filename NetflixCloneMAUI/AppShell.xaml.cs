11b364aa5c5022f60eae24015c73c00802b09274
Jan <Jan@DESKTOP-4QO99K6>
2024-06-11 03:42:29 +0300

    Add project files.

---
+++ NetflixCloneMAUI/AppShell.xaml.cs
@@ -0,0 +1,14 @@
+﻿using NetflixCloneMAUI.Pages;
+
+namespace NetflixCloneMAUI;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+
+        Routing.RegisterRoute(nameof(CategoriesPage), typeof(CategoriesPage));
+
+    }
+}
